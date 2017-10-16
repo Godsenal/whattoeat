43d10a64c8814f30c911182d6079f8f882c62803
Godsenal <tmqps78@gmail.com>
2017-10-16 23:18:32 +0900

    random added

--- src/component/Result.js
+++ src/component/Result.js
@@ -66,7 +66,6 @@ class Result extends Component {
   setRandom(match,i){
     let status = i==MAX_RANDOM - 1?'SUCCESS':'WAITING';
     let random = i==MAX_RANDOM - 1?Math.floor(Math.random() * (match.length)) : i % match.length;
-    console.log(random);
     let intervalId = setTimeout(()=> {
       this.props.setFoodResult(status,match[random]);
     }, 200 * i);
